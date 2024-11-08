6f427041b15a1218d687f6b8037c1444e8af44a6
tainntps27099@fpt.edu.vn <tainntps27099@fpt.edu.vn>
2024-11-08 18:35:47 +0700

    Update filter.blade.php

--- resources/views/admin/discounts/component/filter.blade.php
+++ resources/views/admin/discounts/component/filter.blade.php
@@ -1,10 +1,10 @@
-<form action="{{ route('brand.index') }}" method="GET">
+<form action="{{ route('admin.discounts.index') }}" method="GET">
     @if ($config == 'deleted')
         <input type="hidden" name="deleted" value="daxoa">
     @endif
     <div class="fill-deleted">
-        <a class="all" href="{{ route('brand.index') }}"> Tất cả</a> |
-        <a class="trash" href="{{ route('brand.index', ['deleted' => 'daxoa']) }}">Thùng rác <span>({{ $countDeleted }})</span></a>
+        <a class="all" href="{{ route('admin.discounts.index') }}"> Tất cả</a> |
+        <a class="trash" href="{{ route('admin.discounts.index', ['deleted' => 'daxoa']) }}">Thùng rác <span>({{ $countDeleted }})</span></a>
     </div>
 
     <div class="flex items-center justify-between gap-10 flex-wrap">
@@ -33,7 +33,7 @@
             </div>
         </div>
 
-        <a class="tf-button style-1 w208" href="{{ route('brand.create') }}">
+        <a class="tf-button style-1 w208" href="{{ route('admin.discounts.create') }}">
             <i class="icon-plus"></i>Thêm mới
         </a>
     </div>
